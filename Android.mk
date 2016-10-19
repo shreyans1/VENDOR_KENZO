b866274f1627551d8457866fdc3b1cbc4d4d9092
Shreyans Jain <shreyans.jain.cer14@iitbhu.ac.in>
2016-10-19 17:36:55 +0530

    Fix Fast

--- Android.mk
+++ Android.mk
@@ -156,14 +156,7 @@ LOCAL_MODULE_CLASS := JAVA_LIBRARIES
 LOCAL_MODULE_SUFFIX := .jar
 include $(BUILD_PREBUILT)
 
-include $(CLEAR_VARS)
-LOCAL_MODULE := ims-common
-LOCAL_MODULE_OWNER := xiaomi
-LOCAL_SRC_FILES := proprietary/framework/ims-common.jar
-LOCAL_MODULE_TAGS := optional
-LOCAL_MODULE_CLASS := JAVA_LIBRARIES
-LOCAL_MODULE_SUFFIX := .jar
-include $(BUILD_PREBUILT)
+
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := imscmlibrary
